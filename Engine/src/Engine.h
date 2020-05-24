551c0ea9be1534080a0da15b30dcca275980b029
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-05-24 06:34:27 +0200

    Added logging library

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -3,6 +3,7 @@
 // For use by applications
 
 #include "Engine/Application.h";
+#include "Engine/Log.h"
 
 // --- ENTRY POINT --------------------
 #include "Engine/EntryPoint.h"
--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -3,11 +3,18 @@
 #ifdef ENGINE_PLATFORM_WINDOWS
 
 #include "Application.h"
+#include "Log.h"
 
 extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
+
+	LOG_CORE_WARN("Initialized Log");
+	int a = 5;
+	LOG_INFO("Hello. a = {0}", a);
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
