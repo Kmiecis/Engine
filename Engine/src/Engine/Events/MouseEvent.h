050b1e2e9565147f55df8ccd083d3a657f938339
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-05-28 05:41:28 +0200

    Revert "Revert "Set window callbacks""
    This reverts commit 8683b954ca6080245c25fc86484f15fc1b01e6b0.

--- Engine/src/Engine/Events/MouseEvent.h
+++ Engine/src/Engine/Events/MouseEvent.h
@@ -46,7 +46,7 @@ namespace Engine
 		{
 			std::stringstream ss;
 			ss << "MouseScrolledEvent: " << m_XOffset << ", " << m_YOffset;
-			ss.str();
+			return ss.str();
 		}
 
 		EVENT_CLASS_TYPE(MouseScrolled)
--- Engine/src/Platform/Windows/WindowsWindow.cpp
+++ Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -1,10 +1,18 @@
 #include "WindowsWindow.h"
 
 #include <Engine\Log.h>
+#include <Engine\Events\ApplicationEvent.h>
+#include <Engine\Events\MouseEvent.h>
+#include <Engine\Events\KeyEvent.h>
 
 namespace Engine
 {
 	static bool s_GLFWInitialized = false;
+
+	static void GLFWErrorCallback(int error, const char* desc)
+	{
+		LOG_CORE_ERROR("GLFW Error ({0}): {1}", error, desc);
+	}
 
 	Window* Window::Create(const WindowProperties& properties)
 	{
@@ -35,6 +43,8 @@ namespace Engine
 			int success = glfwInit();
 			LOG_CORE_ASSERT(success, "Could not initialize GLFW");
 
+			glfwSetErrorCallback(GLFWErrorCallback);
+
 			s_GLFWInitialized = true;
 		}
 
@@ -43,6 +53,92 @@ namespace Engine
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 
 		SetVSync(true);
+
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent event(width, height);
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			WindowCloseEvent event;
+			data.EventCallback(event);
+		});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scannode, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent event((float)xoffset, (float)yoffset);
+			data.EventCallback(event);
+		});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double x, double y)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent event((float)x, (float)y);
+			data.EventCallback(event);
+		});
 	}
 
 	void WindowsWindow::Shutdown()
