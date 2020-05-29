043edeecb55b7bc1759e47b3afbd860909d497a6
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-05-29 21:57:27 +0200

    Set imgui events

--- Engine/src/Engine/ImGui/ImGuiLayer.h
+++ Engine/src/Engine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Engine/Core.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/KeyEvent.h"
+#include "Engine/Events/MouseEvent.h"
 #include "Engine/Layer.h"
 
 namespace Engine
@@ -15,6 +18,16 @@ namespace Engine
 		void OnDetach() override;
 		void OnUpdate() override;
 		void OnEvent(Event& e) override;
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 
 	private:
 		float m_Time = 0.0f;
