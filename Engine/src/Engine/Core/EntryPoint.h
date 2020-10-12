367db76827b7e56321fde7d79d15cd1c3d05a205
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-10-12 15:21:33 +0200

    Set visual profiling

--- Engine/src/Engine/Core/EntryPoint.h
+++ Engine/src/Engine/Core/EntryPoint.h
@@ -4,6 +4,7 @@
 
 #include "Engine/Core/Application.h"
 #include "Engine/Core/Log.h"
+#include "Engine/Debug/Instrumentor.h"
 
 extern Engine::Application* Engine::CreateApplication();
 
@@ -11,9 +12,17 @@ int main(int argc, char** argv)
 {
 	Engine::Log::Init();
 
+	ENGINE_PROFILE_BEGIN_SESSION("Startup", "EngineProfile-Startup.json");
 	auto app = Engine::CreateApplication();
+	ENGINE_PROFILE_END_SESSION();
+
+	ENGINE_PROFILE_BEGIN_SESSION("Runtime", "EngineProfile-Runtime.json");
 	app->Run();
+	ENGINE_PROFILE_END_SESSION();
+
+	ENGINE_PROFILE_BEGIN_SESSION("Shutdown", "EngineProfile-Shutdown.json");
 	delete app;
+	ENGINE_PROFILE_END_SESSION();
 
 	return 0;
 }
