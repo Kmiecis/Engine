70c00723974efbf4ca211c6cff61fe586fa93452
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-10-20 06:12:14 +0200

    Revert "Revert "Textures Batch Rendering""
    This reverts commit 7ae2212751651649b07aaad966059c971b7453d1.

--- Engine/src/Engine/Platform/OpenGL/OpenGLShader.h
+++ Engine/src/Engine/Platform/OpenGL/OpenGLShader.h
@@ -30,6 +30,7 @@ namespace Engine
 		virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
 		virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
 		virtual void SetMat4(const std::string& name, const glm::mat4& value) override;
+		virtual void SetIntArray(const std::string& name, int* values, uint32_t count) override;
 
 		void UploadUniformInt(const std::string& name, int value);
 		void UploadUniformFloat(const std::string& name, float value);
@@ -38,6 +39,7 @@ namespace Engine
 		void UploadUniformFloat4(const std::string& name, const glm::vec4& value);
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+		void UploadUniformIntArray(const std::string& name, int* values, uint32_t count);
 
 	private:
 		int GetUniformLocation(const std::string& name) const;
