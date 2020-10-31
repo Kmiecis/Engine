bfe860ea13e034e42de58b1aba7ac54de14d4e47
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-10-31 12:19:04 +0100

    Refactored Input class

--- Engine/src/Engine/Core/Input.h
+++ Engine/src/Engine/Core/Input.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Engine/Core/Core.h"
 #include "Engine/Core/KeyCodes.h"
 #include "Engine/Core/MouseCodes.h"
 
@@ -8,33 +7,13 @@ namespace Engine
 {
 	class Input
 	{
-	protected:
-		Input() = default;
-
 	public:
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
+		static bool IsKeyPressed(KeyCode key);
+		static bool IsMouseButtonPressed(MouseCode button);
 
-		inline static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
+		static std::pair<float, float> GetMousePosition();
 
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-		static Scope<Input> Create();
-
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode key) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-	private:
-		static Scope<Input> s_Instance;
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
