f982b914c9ec4332dd9a42392da485c5a1816834
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-09-12 22:24:03 +0200

    Renamed Core.h to Base.h

--- Engine/src/Engine/Core/Base.h
+++ Engine/src/Engine/Core/Base.h
@@ -44,12 +44,20 @@
 #endif // End of platform detection
 
 #ifdef NG_DEBUG
+    #if defined(NG_PLATFORM_WINDOWS)
+        #define NG_DEBUGBREAK() __debugbreak()
+    #elif defined (NG_PLATFORM_LINUX)
+        #include <signal.h>
+        #define NG_DEBUGBREAK() raise(SIGTRAP)
+    #else
+        #error "Platform doesn't support debugbreak"
+    #endif
     #define NG_ENABLE_ASSERTS
 #endif
 
 #ifdef NG_ENABLE_ASSERTS
-    #define NG_ASSERT(x, ...) { if(!(x)) { NG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-    #define NG_CORE_ASSERT(x, ...) { if(!(x)) { NG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define NG_ASSERT(x, ...) { if(!(x)) { NG_ERROR("Assertion Failed: {0}", __VA_ARGS__); NG_DEBUGBREAK(); } }
+    #define NG_CORE_ASSERT(x, ...) { if(!(x)) { NG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); NG_DEBUGBREAK(); } }
 #else
     #define NG_ASSERT(x, ...)
     #define NG_CORE_ASSERT(x, ...)
--- Engine/src/Engine/Renderer/SubTexture2D.h
+++ Engine/src/Engine/Renderer/SubTexture2D.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Engine/Core/Core.h"
+#include "Engine/Core/Base.h"
 #include "Engine/Renderer/Texture.h"
 
 #include <glm/glm.hpp>
