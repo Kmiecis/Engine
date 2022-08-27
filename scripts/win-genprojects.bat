a8ce35883101983eb64475a5abe6952fc603a509
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-08-27 13:46:50 +0200

    Changed submodules to be TheCherno free

--- scripts/win-genprojects.bat
+++ scripts/win-genprojects.bat
@@ -1,5 +1,11 @@
 @echo off
 pushd ..\
+xcopy premakes\GLFW\premake5.lua Engine\vendor\GLFW
+xcopy premakes\imgui\premake5.lua Engine\vendor\imgui
+xcopy premakes\yaml-cpp\premake5.lua Engine\vendor\yaml-cpp
 call scripts\premake5.exe vs2022
+del Engine\vendor\GLFW\premake5.lua
+del Engine\vendor\imgui\premake5.lua
+del Engine\vendor\yaml-cpp\premake5.lua
 popd
 PAUSE
